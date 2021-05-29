ab5e8ce1dd8b04745c80209844b60ace478e469e
SergeyZheleznov <jeleznov_sv@mail.ru>
2021-05-29 18:03:05 +0300

    Правка 2 Задание 2.14 ветка module2-task1 дата 18.02.2021

--- js/main.js
+++ js/main.js
@@ -1,29 +1,27 @@
 //Функция, возвращающая случайное целое число из переданного диапазона включительно
 //https://habr.com/ru/company/ruvds/blog/534108/
 
-const res1;
-let res2;
-
-function randomInteger(min,max) {
-  if (min => max && min < 0) {
+const getRandomInteger = (min,max) => {
+  if (min > max || min < 0) {
     alert('Произошла ошибка при вводе диапазона, либо минимальное значение больше максимального или равно ему, либо задан отрицательный диапазон, введете диапазон правильно');
   } else {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 };
 
+console.log(getRandomInteger(9, 10));
+
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 
+let res2 = 0;
 
-function randomDig(min, max, numbersAfterComma) {
-  if (min => max && min < 0) {
-    alert('ППроизошла ошибка при вводе диапазона, либо минимальное значение больше максимального или равно ему, либо задан отрицательный диапазон, введете диапазон правильно');
+const getRandomDig = (min, max, numbersAfterComma) => {
+  if (min > max) {
+    alert('Произошла ошибка при вводе диапазона, либо минимальное значение больше максимального или равно ему, либо задан отрицательный диапазон, введете диапазон правильно');
   } else {
     res2 = Math.random() * (max - min) + min;
     return res2.toFixed(numbersAfterComma);
   }
 };
 
-res1 = randomInteger (1,4);
-
-res2 = randomDig (10, 11, 6);
+console.log (getRandomDig(6, 11, 3));
