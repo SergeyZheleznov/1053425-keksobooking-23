1f5fc069517b200c90b1cc1f4748316113a3d7f8
SergeyZheleznov <jeleznov_sv@mail.ru>
2021-05-29 18:07:26 +0300

    Правка 3 Задание 2.14 ветка module2-task1 дата 18.02.2021

--- js/main.js
+++ js/main.js
@@ -9,19 +9,17 @@ const getRandomInteger = (min,max) => {
   }
 };
 
-console.log(getRandomInteger(9, 10));
+console.log(getRandomInteger(8, 10));
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-
-let res2 = 0;
 
 const getRandomDig = (min, max, numbersAfterComma) => {
   if (min > max) {
     alert('Произошла ошибка при вводе диапазона, либо минимальное значение больше максимального или равно ему, либо задан отрицательный диапазон, введете диапазон правильно');
   } else {
-    res2 = Math.random() * (max - min) + min;
+    const res2 = Math.random() * (max - min) + min;
     return res2.toFixed(numbersAfterComma);
   }
 };
 
-console.log (getRandomDig(6, 11, 3));
+console.log (getRandomDig(8, 11, 3));
