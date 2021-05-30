d70e768f561c701640b9cedc4c88ed221646bae4
SergeyZheleznov <jeleznov_sv@mail.ru>
2021-05-30 09:44:47 +0300

    Правка 4 задание 2.14 ветка module2-task1 дата 30.05.2021

--- js/main.js
+++ js/main.js
@@ -2,24 +2,33 @@
 //https://habr.com/ru/company/ruvds/blog/534108/
 
 const getRandomInteger = (min,max) => {
-  if (min > max || min < 0) {
-    alert('Произошла ошибка при вводе диапазона, либо минимальное значение больше максимального или равно ему, либо задан отрицательный диапазон, введете диапазон правильно');
+  if (min > max) {
+    let res = 0;
+    res = min;
+    min = max;
+    max = res;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   } else {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 };
 
-console.log(getRandomInteger(8, 10));
+getRandomInteger (1,4);
 
 //Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 
 const getRandomDig = (min, max, numbersAfterComma) => {
   if (min > max) {
-    alert('Произошла ошибка при вводе диапазона, либо минимальное значение больше максимального или равно ему, либо задан отрицательный диапазон, введете диапазон правильно');
+    let res = 0;
+    res = min;
+    min = max;
+    max = res;
+    const res2 = Math.random() * (max - min) + min;
+    return res2.toFixed(numbersAfterComma);
   } else {
     const res2 = Math.random() * (max - min) + min;
     return res2.toFixed(numbersAfterComma);
   }
 };
 
-console.log (getRandomDig(8, 11, 3));
+getRandomDig(2,4,3);
