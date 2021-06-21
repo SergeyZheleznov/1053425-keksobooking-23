63ce8434d469c6f204fc14c9ce4e969deea70f60
SergeyZheleznov <jeleznov_sv@mail.ru>
2021-06-21 18:00:48 +0300

    Правка 6 Задание 7.9 ветка module7-task1 от 21.06.2021

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,4 @@
-/*import {OFFER} from './util';
-import {createAuthor} from './util';
+import {OFFER} from './util.js';
 
 //Достаю из шаблона его содержимое
 const templateFragment = document.querySelector('#card').content;
@@ -12,15 +11,27 @@ const element = templateFragment.cloneNode(true);
 //Выполняю первую подзадачу задания № 2
 //element Это единственнный разумно доступный дом элемент у меня сейчас
 
+if (OFFER.offer.title === undefined) {
+  document.querySelector('.popup__title').classList.add ('hidden');
+}
+
 element.querySelector('.popup__title').textContent = OFFER.offer.title;
 
 //Выполняю вторую подзадачу задания № 2
+
+if (OFFER.offer.address === undefined) {
+  document.querySelector('.popup__address').classList.add ('hidden');
+}
 
 element.querySelector('.popup__text--address').textContent = OFFER.offer.address;
 
 //Выполняю третью подзадачу задания № 2
 //Выведите цену offer.price в блок .popup__text--price строкой вида
 //{{offer.price}} ₽/ночь. Например, «5200 ₽/ночь».
+
+if (OFFER.offer.price === undefined) {
+  document.querySelector('.popup__price').classList.add ('hidden');
+}
 
 element.querySelector('.popup__text--price').textContent = `${OFFER.offer.price} ₽/ночь`;
 
@@ -52,6 +63,10 @@ switch (OFFER.offer.type) {
     break;
 }
 
+if (OFFER.offer.type === undefined) {
+  document.querySelector('.popup__type').classList.add ('hidden');
+}
+
 element.querySelector('.popup__type').textContent = OFFER.offer.type;
 
 //четвёртая подзадача задания № 2 решена
@@ -61,6 +76,14 @@ element.querySelector('.popup__type').textContent = OFFER.offer.type;
 //строкой вида {{offer.rooms}} комнаты для {{offer.guests}} гостей.
 //Например, «2 комнаты для 3 гостей».
 
+if (OFFER.offer.rooms === undefined) {
+  document.querySelector('.popup__text--capacity').classList.add ('hidden');
+}
+
+if (OFFER.offer.guests === undefined) {
+  document.querySelector('.popup__text--capacity').classList.add ('hidden');
+}
+
 element.querySelector('.popup__text--capacity').textContent = `${OFFER.offer.rooms} комнаты для ${OFFER.offer.guests} гостей`;
 // Пятая подзадача задания № 2 успешно решена
 
@@ -68,6 +91,14 @@ element.querySelector('.popup__text--capacity').textContent = `${OFFER.offer.roo
 // Время заезда и выезда offer.checkin и offer.checkout в блок
 // .popup__text--time строкой вида Заезд после {{offer.checkin}},
 // выезд до {{offer.checkout}}. Например, «Заезд после 14:00, выезд до 14:00»
+
+if (OFFER.offer.checkin === undefined) {
+  document.querySelector('.popup__text--time').classList.add ('hidden');
+}
+
+if (OFFER.offer.checkout === undefined) {
+  document.querySelector('.popup__text--time').classList.add ('hidden');
+}
 
 element.querySelector('.popup__text--time').textContent = `Заезд до ${OFFER.offer.checkin} выезд после ${OFFER.offer.checkout}`;
 
@@ -99,6 +130,10 @@ for (let index = 0; index < OFFER.offer.features.length; index++) {
 //В блок .popup__description
 //выведите описание объекта недвижимости offer.description.
 
+if (OFFER.offer.description === undefined) {
+  document.querySelector('.popup__description').classList.add ('hidden');
+}
+
 element.querySelector('.popup__description').textContent = OFFER.offer.description;
 
 //Девятая подзадача задания № 2
@@ -127,12 +162,14 @@ for (let index = 0; index < OFFER.offer.photos.length; index++) {
 //Замените значение атрибута src у аватарки пользователя
 // .popup__avatar на значение поля author.avatar.
 
-const authorCreate = createAuthor();
-
-element.querySelector('.popup__avatar').src = authorCreate.author.avatar;
+element.querySelector('.popup__avatar').src = OFFER.author.avatar;
 
 //Задача успешно решена
 
 //находит по селектору домэлемент и вставляет туда содержимое шаблона (тейплейта)
 document.querySelector('#map-canvas').appendChild(element);
-*/
+
+//Последнее требование в задаче №2
+//Предусмотрите ситуацию, когда данных для заполнения не хватает.
+//Например, отсутствует описание. В этом случае соответствующий блок
+// в карточке скрывается.
