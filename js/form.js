aa683f0f603e05654fc68647f8c14ac1b08bffb0
SergeyZheleznov <jeleznov_sv@mail.ru>
2021-06-26 09:40:42 +0300

    Правка 2 задание 8.10 ветка module8-task1 от 26.06.2021

--- js/form.js
+++ js/form.js
@@ -14,10 +14,87 @@
 // добавление элементам option состояния disabled или другие способы ограничения,
 // например, с помощью метода setCustomValidity.
 
-//Этот обработчик с ошибкой, не работает.
-/*const form = document.querySelector('.ad-form');
-form.addEventListener('sumbit', function () {
+// нужно вызывать метод setCustomValidity у нужного элемента с нужным текстом,
+// что бы при отправке неправильно заполненной формы сообщение об ошибке выскакивало
+
+const selectRooms = document.querySelector('#room_number');
+const selectCapacity = document.querySelector('#capacity');
+
+selectRooms.addEventListener('change', () => {
+  if (selectRooms.value === '1') {
+    selectCapacity.setCustomValidity('Одна комната только для одного гостя');
+  } else if (selectRooms.value === '2') {
+    selectCapacity.setCustomValidity('Две комнаты только для одного гостя или для двух гостей');
+  } else if (selectRooms.value === '3') {
+    selectCapacity.setCustomValidity('Две комнаты только для одного гостя, для двух гостей или трёх гостей');
+  } else if (selectRooms.value === '100') {
+    selectCapacity.setCustomValidity('не для гостей');
+  } else {
+    selectCapacity.setCustomValidity('');
+  }
+
+  selectCapacity.reportValidity();
+});
+
+/*
+const selectRooms = document.querySelector('#room_number'); //передал селект с комнатами в переменную
+
+const elementOption3 = document.querySelector('#capacity3');
+console.log(elementOption3);
+const elementOption2 = document.querySelector('#capacity2');
+const elementOption1 = document.querySelector('#capacity1');
+const elementOption0 = document.querySelector('#capacity0');
+
+selectRooms.addEventListener('change', function () { //повесил на этот селект обработчик,
+  // если там что-то, то он сработает
+  const roomsNumber = this.value;
+  console.log (roomsNumber);
+  if (roomsNumber == '1') {
+    elementOption3.disabled = true;
+    elementOption2.disabled = true;
+    elementOption1.disabled = false;
+    elementOption0.disabled = true;
+    console.log (elementOption3);
+    console.log (elementOption2);
+    console.log (elementOption1);
+    console.log (elementOption0);
+  }  else if (roomsNumber == '2' ){
+    elementOption3.disabled = true;
+    elementOption2.disabled = false;
+    elementOption1.disabled = false;
+    elementOption0.disabled = true;
+    console.log (elementOption3);
+    console.log (elementOption2);
+    console.log (elementOption1);
+    console.log (elementOption0);
+  } else if (roomsNumber == '3'){
+    elementOption3.disabled = false;
+    elementOption2.disabled = false;
+    elementOption1.disabled = false;
+    elementOption0.disabled = true;
+    console.log (elementOption3);
+    console.log (elementOption2);
+    console.log (elementOption1);
+    console.log (elementOption0);
+  } else if (roomsNumber == '100'){
+    elementOption3.disabled = false;
+    elementOption2.disabled = false;
+    elementOption1.disabled = false;
+    elementOption0.disabled = true;
+    console.log (elementOption3);
+    console.log (elementOption2);
+    console.log (elementOption1);
+    console.log (elementOption0);
+  }
+});
+
 */
+
+//Этот обработчик с ошибкой, не работает.
+/*
+const form = document.querySelector('.ad-form');
+form.addEventListener('sumbit', function () {
+
 const listRooms = document.querySelector('#capacity');
 
 listRooms.innerHTML = '';
@@ -26,7 +103,7 @@ const elementOption2 = document.createElement('option');
 const elementOption3 = document.createElement('option');
 const elementOption100 = document.createElement('option');
 
-const select = document.querySelector('#room_number'); //передал селект с комнатами в переменную
+const select = document.querySelector('#room_number');
 
 select.addEventListener('change', function () {
   const roomsNumber = this.value;
@@ -63,3 +140,4 @@ select.addEventListener('change', function () {
       break;
   }
 });
+*/
